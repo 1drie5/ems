69d62d859cb25e0ed4034194d470f4b4564afba0
Idries Ahamed K A <idriesahamedka@gmail.com>
2026-06-23 19:50:33 +0530

    add employee management modals and form UI

--- client/src/components/EmployeeCard.jsx
+++ client/src/components/EmployeeCard.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { PencilIcon, Trash2Icon } from 'lucide-react'
 const EmployeeCard = ({employee, onDelete, onEdit}) => {
+  const handleDelete = async ()=>{
+    if(!confirm("Are you sure you want to delete this employee?")) return; 
+  }
   return (
     <div className='group relative card card-hover overflow-hidden'>
-        <div className='relative aspect-4/3 w-full overflow-hidden bg-linear-to-br from-slate-100 to-skate-50'>
+        <div className='relative aspect-4/3 w-full overflow-hidden bg-linear-to-br from-slate-100 to-slate-50'>
           
           <div className='w-full h-full flex items-center justify-center'>
             {/* circle icons */}
@@ -23,7 +26,7 @@ const EmployeeCard = ({employee, onDelete, onEdit}) => {
         {!employee.isDeleted && (
           <div className='absolute inset-0 bg-linear-to-t from-indigo-700/20 via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity flex items-end justify-center pb-6 gap-3'>
             <button onClick={()=> onEdit(employee)} className='p-2.5 bg-white/90 backdrop-blur-sm text-slate-700 hover:text-indigo-600 rounded-xl shadow-lg transiition-all hover:scale-105'><PencilIcon className='w-4 h-4'/></button>
-            <button className='p-2.5 bg-white/90 backdrop-blur-sm text-slate-700 hover:text-rose-600 rounded-xl shadow-lg transition-all hover:scale-105 disabled:opacity-50'><Trash2Icon className='w-4 h-4'/></button>
+            <button onClick={handleDelete} className='p-2.5 bg-white/90 backdrop-blur-sm text-slate-700 hover:text-rose-600 rounded-xl shadow-lg transition-all hover:scale-105 disabled:opacity-50'><Trash2Icon className='w-4 h-4'/></button>
           </div>
         )}
 
